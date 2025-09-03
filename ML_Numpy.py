61a5fa13ab7912e72577e83b3715929c72323bac
baazizkhadidja <baazize-khadija@hotmail.com>
2025-09-03 19:53:25 +0200

    base numpy

--- ML_Numpy.py
+++ ML_Numpy.py
@@ -17,10 +17,28 @@ D
 
 arr = np.random.randn(3, 4)
 
-
 notes = np.random.normal(loc=12, scale=2, size=100)
 
 np.eye(4)
 
 np.linspace(0, 10, 20, dtype=np.float16 )
 
+
+
+A= np.zeros((3,2))
+
+B = np.ones((3, 2))
+
+print(A)
+
+print(B)
+
+C1 = np.hstack((A,B))
+C1
+
+C2 = np.vstack((A, B))
+C2
+
+C3 = np.concatenate((A, B), axis=0)
+C3
+C4 = np.concatenate(, ()
