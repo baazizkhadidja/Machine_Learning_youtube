61dd57d6d533caa4e14d30d649fdca54a0926c09
baazizkhadidja <baazize-khadija@hotmail.com>
2025-09-03 19:52:08 +0200

    base numpy

---
+++ ML_Numpy.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+
+A = np.array([1, 2, 3])
+A.shape
+
+B = np.zeros((3,2))
+B.ndim
+B.shape
+
+C = np.ones((3, 5))
+
+C.size
+
+D = np.full((2, 3), 9)
+D
+
+arr = np.random.randn(3, 4)
+
+
+notes = np.random.normal(loc=12, scale=2, size=100)
+
+np.eye(4)
+
+np.linspace(0, 10, 20, dtype=np.float16 )
+
